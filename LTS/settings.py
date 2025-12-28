b04c498ef5ed4bc94b2ba99377a5fbfc8ee1dd2c
Achaudhari02 <ashutoshc018@gmail.com>
2025-12-28 12:35:09 -0500

    user login and logout implemented

--- LTS/settings.py
+++ LTS/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'DIRS': [BASE_DIR / "templates"],
     },
 ]
 
@@ -116,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'base'
+LOGOUT_REDIRECT_URL = 'login'
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import request
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-def baseView(request):
-    return render(request, 'base.html')
+def base(request):
+    return render(request, 'inventory/home.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username=username, password=password)
+        if user is not None: 
+            login(request,user)
+            return redirect('base')
+        else: 
+            messages.error(request, "The user does not exist") 
+            
+
+            
+    return render(request, 'accounts/login.html')
+
+@login_required
+def logout_user(request):
+    if request.method == 'GET':
+        logout(request)
+    return redirect('login')
